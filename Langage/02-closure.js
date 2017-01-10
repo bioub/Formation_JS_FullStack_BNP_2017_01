791d09661e5c0bdd3dda2914578cae8d5dde776e
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2017-01-10 17:01:10 +0100

    Jour 1

---
+++ Langage/02-closure.js
@@ -0,0 +1,33 @@
+var logClosure = function(msg) {
+    // portée de closure (sauvegardée)
+    return function() {
+        return msg;
+    };
+};
+
+var logBonjour = logClosure('Bonjour');
+console.log(logBonjour());
+
+var logCoucou = logClosure('Coucou');
+console.log(logCoucou());
+
+var timeoutClosure = function() {
+    setTimeout(function() {
+        
+    }, 1000);
+};
+
+var prenoms = ['Romain', 'Jean'];
+
+for (var i=0; i<prenoms.length; i++) {
+    var prenom = prenoms[i];
+    setTimeout(function() {
+        console.log(prenom);
+    }, 1000);
+}
+
+prenoms.forEach(function(prenom) {
+    setTimeout(function() {
+        console.log(prenom);
+    }, 1000);
+});
---
+++ Langage/test.js
@@ -0,0 +1,7 @@
+var space = Number(' ');
+console.log(space);
+console.log(Number.isNaN(space));
+
+var space = Number.parseInt(' ');
+console.log(space);
+console.log(Number.isNaN(space));
